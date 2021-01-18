0f1c07308187299f547efe44e549dab26fdcf2a1
Renat <restspread@gmail.com>
2021-01-18 23:17:11 +0300

    add components

---
+++ src/components/post-add-form/post-add-form.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PostListItem from '../post-list-item';
+
+const PostAddForm = () => {
+	return (
+		<form className="bottom-panel d-flex">
+			<input
+				className="" 
+				type="text"
+				placeholder="О чем вы думаете сейчас?"
+			/>
+			<button 
+				className="btn btn-outline-secondary"
+				type="submit">
+				Добавить
+			</button>
+		</form>
+	)
+}
+
+export default PostAddForm;
